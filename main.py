d93c7ae58e499c6bd35a7e38e23563245eead1b6
Ailish <snickrrdoodles@gmail.com>
2025-03-19 08:15:34 -0400

    Basic setup and layouts

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
 @app.route('/boats')
@@ -16,21 +16,29 @@ def boats():
     boats = conn.execute(text('SELECT * FROM boats')).all()
     return render_template('boats.html', boats = boats[:10])
 
-@app.route('/boatCreate', methods = ['GET'])
+@app.route('/create', methods = ['GET'])
 def getBoat():
     return render_template('boat_create.html')
 
-@app.route('/boatCreate', methods = ['POST'])
+@app.route('/create', methods = ['POST'])
 def createBoat():
     try:
         conn.execute(text('INSERT INTO boats VALUES(:id, :name, :type, :owner_id, :rental_price)'), request.form)
         return render_template('boat_create.html', error = None, success = "Successful")
     except:
         return render_template('boat_create.html', error = "Failed", success = None)
+    
+@app.route('/update')
+def update():
+    return render_template('boats_update.html')
 
-@app.route('/<name>')
-def greeting(name):
-    return render_template('user.html', user = name) # var "user" is passed from user.html
+@app.route('/search')
+def search():
+    return render_template('boats_search.html')
+
+@app.route('/delete')
+def delete():
+    return render_template('boats_delete.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
